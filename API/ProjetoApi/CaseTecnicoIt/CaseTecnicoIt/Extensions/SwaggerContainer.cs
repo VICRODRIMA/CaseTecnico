92746a1133b7193c06ee6936160217724c517bf7
Victor Matos <vicrodrima@gmail.com>
2020-09-07 10:13:24 -0300

    inclusao api

---
+++ API/ProjetoApi/CaseTecnicoIt/CaseTecnicoIt/Extensions/SwaggerContainer.cs
@@ -0,0 +1,38 @@
+﻿namespace CaseTecnicoIt.Extensions
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.OpenApi.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    namespace CaseTecnicoIt.Extensions
+    {
+        internal static class SwaggerContainer
+        {
+            private const string repo = "https://github.com/VICRODRIMA/CaseTecnico";
+
+            public static IServiceCollection AddSwagger(this IServiceCollection services)
+            {
+                services.AddSwaggerGen(options =>
+                {
+                    options.SwaggerDoc("v1", new OpenApiInfo
+                    {
+                        Title = "API Case Tecnico",
+                        Version = "v1",
+                        Description = "api case tecnico itau para vag",
+                        Contact = new OpenApiContact
+                        {
+                            Url = new Uri(repo),
+                            Name = "Victor Matos",
+                            Email = "vicrodrimap@gmail.com"
+                        }
+                    });
+                });
+
+                return services;
+            }
+        }
+    }
+}
